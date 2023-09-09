c9612d8f7e05164e482be45b7b70940eab6c2fbf
rosom <yuoran371@gmail.com>
2023-09-09 16:38:42 +0300

    Add finished first task (EPAM)

--- src/main/java/com/epam/mjc/collections/map/WordRepetitionMapCreator.java
+++ src/main/java/com/epam/mjc/collections/map/WordRepetitionMapCreator.java
@@ -1,8 +1,25 @@
 package com.epam.mjc.collections.map;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class WordRepetitionMapCreator {
     public Map<String, Integer> createWordRepetitionMap(String sentence) {
+    	String[] a =sentence.toLowerCase().split("\\s+|\\.|(\\,\\s)"); 
+    	Map <String, Integer> hm = new HashMap<>();
+    	if (sentence.isEmpty()) {
+			return hm;
+		}
+
+    	for (int i=0; i<a.length; i++) {
+    		if (hm.containsKey(a[i])) {
+        	hm.put(a[i], hm.get(a[i])+1);
+    		} 
+    		 else {
+        		hm.put(a[i], 1);
+        		}
+    	}
+
+		return hm;
     }
 }
